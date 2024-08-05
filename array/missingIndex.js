ff6dd67040a519078c4393a4dc7c3c17ae97f9ef
prince-nkuru <nkuruprinces@gmail.com>
2024-08-05 17:18:34 +0530

    add alternative

--- array/missingIndex.js
+++ array/missingIndex.js
@@ -1,3 +1,10 @@
 function isAllPossibilities(x){
 	return x.length > 0 ? x.every((a,i) => x.includes(i)) : false;
 }
+
+///////////////////////////////////////////////////
+
+function isAllPossibilities(a){
+  return a.length > 0 && a.every((x,i) => a.includes(i));
+}
+
