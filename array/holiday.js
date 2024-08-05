d4e27faf8650028b7b086f5923569dad33b3db96
prince-nkuru <nkuruprinces@gmail.com>
2024-08-05 16:52:16 +0530

    add pack to it

--- array/holiday.js
+++ array/holiday.js
@@ -4,3 +4,5 @@ In Bali, as far as I can gather, when ex-pats speak to locals, they basically in
 Anyway, as some basic language education(??) this kata requires you to turn any sentence provided (s) into ex-pat balinese waffle by inserting the word 'pak' between every other word. Simple 8kyu :D
 
 Pak should not be the first or last word. Strings of just spaces should return an empty string.*/
+const pak = s =>
+  s.trim().split(` `).join(` pak `);
