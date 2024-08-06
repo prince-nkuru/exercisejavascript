16139af09aa134b3bfb029327b2d58ae50ffd768
prince-nkuru <nkuruprinces@gmail.com>
2024-08-06 11:11:46 +0530

    submitting an answer

--- array/lastEven.js
+++ array/lastEven.js
@@ -5,3 +5,8 @@ For example:
 ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
 ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
 ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]*/
+
+function evenNumbers(array, number) {
+  let arr = array.filter(n => n % 2 === 0);
+  return arr.slice(-number)
+}
