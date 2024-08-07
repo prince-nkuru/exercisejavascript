e9f6aad589ffef236d091d43c9cc1d2066823137
prince-nkuru <nkuruprinces@gmail.com>
2024-08-07 21:08:31 +0530

    added solution

--- array/jenny.js
+++ array/jenny.js
@@ -7,3 +7,16 @@ Spaces are not places, you need the actual letters. No spaces.
 The returned word should be all lowercase letters.
 if you can't find one of the letters using the index numbers, return "No mission today". Jenny would be very sad, but that's life... :(
 Example: input: [5, 0, 3], "I Love You" output: "ivy" (0 = "i", 3 = "v", 5 = "y")*/
+function missingWord(nums, str) {
+  var result = "";
+  var string = str.split(" ").join("");
+  nums.sort((a,b) => a - b);
+  nums.forEach(function(x){ 
+    if(string[x]){
+      result += string[x].toLowerCase();
+    }else{
+      result = "No mission today";
+    }
+  });
+  return result;
+}
