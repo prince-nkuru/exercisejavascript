f8e236e122b25d104bc3b9e458e1246e9269a99f
prince-nkuru <nkuruprinces@gmail.com>
2024-08-08 18:54:38 +0530

    added second way

--- array/dontMutate.js
+++ array/dontMutate.js
@@ -2,3 +2,10 @@ function removeSmallest(numbers) {
   const min = Math.min.apply(this, numbers);
   return numbers.filter((num, idx, arr) => idx !== arr.indexOf(min));
 }
+
+//////////////////////////////////////////////////////////////////
+
+function removeSmallest(numbers) {
+  let indexOfMin = numbers.indexOf(Math.min(...numbers));
+  return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)];
+}
