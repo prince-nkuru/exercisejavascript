bff5f50226b3f025b027e858ee53a9ea644fee74
prince-nkuru <nkuruprinces@gmail.com>
2024-08-08 17:25:34 +0530

    added alf

---
+++ array/alf.js
@@ -0,0 +1,22 @@
+/*Description:
+Late last night in the Tanner household, ALF was repairing his spaceship so he might get back to Melmac. Unfortunately for him, he forgot to put on the parking brake, and the spaceship took off during repair. Now it's hovering in space.
+
+ALF has the technology to bring the spaceship home if he can lock on to its location.
+
+Given a map:
+
+..........
+..........
+..........
+.......X..
+..........
+..........
+The map will be given in the form of a string with \n separating new lines. The bottom left of the map is [0, 0]. X is ALF's spaceship.
+
+In this example:
+
+findSpaceship(map) => [7, 2]
+If you cannot find the spaceship, the result should be
+
+"Spaceship lost forever."
+Can you help ALF?*/
