140c3d5eecd5d2261c75d831e52d000ae0348e97
prince-nkuru <nkuruprinces@gmail.com>
2024-08-14 23:12:32 +0530

    add second commit

--- array/office.js
+++ array/office.js
@@ -14,3 +14,22 @@ function outed(meet, boss) {
   let score = names.reduce((s,v) => s + meet[v], 0) + meet[boss]
   return score / names.length > 5 ? 'Nice Work Champ!' : 'Get Out Now!'
 }
+
+////////////////////////////////////////////////////////////////////////////
+
+function outed(meet, boss){
+  var sum=0;
+  var count=0;
+  for (var key in meet)
+    if (key!=boss)
+    {
+      sum+=meet[key];
+      count++;
+    }  
+    else
+    {
+      sum+=meet[key]*2;
+      count++;
+    }
+  return sum/count<=5?'Get Out Now!':'Nice Work Champ!';
+}
