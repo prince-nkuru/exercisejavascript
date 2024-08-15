88cb3e401a423a9eaaa62ecde4516863e2c2e5eb
prince-nkuru <nkuruprinces@gmail.com>
2024-08-15 09:50:33 +0530

    added second option

--- array/functionl.js
+++ array/functionl.js
@@ -2,3 +2,10 @@ function dropWhile(arr, pred) {
   for (var i = 0; i < arr.length && pred(arr[i]); ++i) ;
   return arr.slice(i)
 }
+
+////////////////////////////////////////////////////////
+
+function dropWhile(arr, pred) {
+  const idx = arr.findIndex(elem => !pred(elem));
+  return idx < 0 ? [] : arr.slice(idx);
+}
