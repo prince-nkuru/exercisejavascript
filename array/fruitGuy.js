23d61b367c2211a5376c95ce8589e91e91cff597
prince-nkuru <nkuruprinces@gmail.com>
2024-08-17 21:11:15 +0530

    added second

--- array/fruitGuy.js
+++ array/fruitGuy.js
@@ -9,3 +9,10 @@ The returned array should be in lowercase.*/
 function removeRotten(arr){
   return arr ? arr.map(x=>x.replace('rotten', '').toLowerCase()) : [] ;
 }
+
+////////////////////////////////
+
+function removeRotten(bagOfFruits){
+  if(!bagOfFruits || !bagOfFruits.length) {return [];} 
+  else {return bagOfFruits.map(e => e.replace(/^rotten/g, '').toLowerCase())};
+}
