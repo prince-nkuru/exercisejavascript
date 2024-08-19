c6e60bd3245e4d9395d67139dd0aab0206e9ee33
prince-nkuru <nkuruprinces@gmail.com>
2024-08-19 22:59:27 +0530

    added second commit -m

--- array/operation.js
+++ array/operation.js
@@ -22,3 +22,13 @@ function calc(a) {
       return a + elt;
     }, 0);
 }
+////////////////////////////////////////////////////////////////////////////
+
+var calc = function(a) {
+  return a.reduce((a,b,i)=>{
+    b = b>0? b**2 : b
+    b = (i+1)%3==0? b*3 : b
+    b = (i+1)%5==0? -b:b
+    return a+b
+  },0)
+}
