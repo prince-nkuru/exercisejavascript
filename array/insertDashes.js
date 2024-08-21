9f34ffb2ab57a63b219d3128e3b5d84f8df57d16
prince-nkuru <nkuruprinces@gmail.com>
2024-08-21 23:06:14 +0530

    adding alternative version

--- array/insertDashes.js
+++ array/insertDashes.js
@@ -22,3 +22,13 @@ num = num.toString().split('')
  return num.join('')
  
 }
+/////////////////////////////////////////////////////////////////
+
+function insertDash(num) {
+  return num.toString().split('').reduce((acc, c, i, arr) => {
+    if (i > 0 && Number(arr[i]) % 2 !== 0 && Number(arr[i - 1]) % 2 !== 0)
+      acc = acc + '-';
+    
+    return acc + c;
+  }, '');
+}
