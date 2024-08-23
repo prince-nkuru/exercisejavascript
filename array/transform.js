d66ed31cf1cc970f91e409a5d508d3b5db5068b1
prince-nkuru <nkuruprinces@gmail.com>
2024-08-23 15:43:49 +0530

    added second

--- array/transform.js
+++ array/transform.js
@@ -26,3 +26,12 @@ function sortTransform(a){
 }
 
 let transform = (arr) => [...arr.slice(0,2), ...arr.slice(arr.length - 2)].map(a => String.fromCharCode(a)).join('');
+
+//////////////////////////////////////////////////////////////
+
+const sortTransform = (arr) => {
+  const letter = el => String.fromCodePoint( ...el.slice(0, 2).concat(el.slice(-2)) )
+  const choose = [...arr].sort((a, b) => a - b)
+  
+  return [letter(arr), letter(choose), letter( [ ...choose ].reverse() ), letter(choose) ].join('-')
+}
