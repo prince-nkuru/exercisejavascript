089fac8319d60425e8d859b07c0fb0d57a6b2fe8
prince-nkuru <nkuruprinces@gmail.com>
2024-08-27 21:11:57 +0530

    added second

--- array/string.js
+++ array/string.js
@@ -5,3 +5,17 @@ All inputs will be valid.*/
 
 const alpha = ' zyxwvutsrqponmlkjihgfedcba!? '
 const switcher = x => x.map(item => alpha[item]).join('')
+
+///////////////////////////////////////////////////////////////
+
+function switcher(x)
+{
+  return x.map(a => 
+    {
+      if(a == 27) return "!";
+      if(a == 28) return "?";
+      if(a == 29) return " ";
+      return String.fromCharCode(97 + 26 - parseInt(a));
+      
+    }).join('');
+}
