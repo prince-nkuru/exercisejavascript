e516c7d00d95d77a384a2c86f613822a89cf677d
prince-nkuru <nkuruprinces@gmail.com>
2024-08-28 08:22:15 +0530

    added third

--- array/small.js
+++ array/small.js
@@ -16,3 +16,9 @@ function min(arr, toReturn) {
 /////////////////////////////////////////////////////////////////////////
 
 const min = (arr, toReturn) => toReturn === 'value' ? Math.min(...arr) : arr.indexOf(Math.min(...arr));
+
+/////////////////////////////////////////////////////////////////////
+
+function min( array, outer ) {
+  return outer === 'index' ? array.indexOf(Math.min(...array)) : Math.min(...array)
+}
