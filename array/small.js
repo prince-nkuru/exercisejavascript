5bec0380e81beee12ea98ab17336fe69019e4967
prince-nkuru <nkuruprinces@gmail.com>
2024-08-28 08:19:39 +0530

    added sol

--- array/small.js
+++ array/small.js
@@ -4,3 +4,12 @@ Assume the first parameter will always be an array filled with at least 1 number
 
 min([1,2,3,4,5], 'value') // => 1
 min([1,2,3,4,5], 'index') // => 0*/
+function min(arr, toReturn) {
+  
+  
+  if (toReturn === 'value'){
+    return Math.min(...arr)
+  }else if (toReturn === 'index'){
+    return arr.indexOf(Math.min(...arr));
+  }
+}
